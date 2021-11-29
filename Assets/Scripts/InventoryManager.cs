230e6fedc4c693f51725c27b1b1b9a82618c3b9a
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2021-11-29 18:31:26 +0100

    pre test save

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [SerializeField] private GameObject inventory;
+    [SerializeField] private GameObject inventoryButton;
+
+    void Start()
+    {
+        CloseInventory();
+    }
+    public void OpenInventory()
+    {
+        inventory.SetActive(true);
+        inventoryButton.SetActive(false);
+    }
+
+    public void CloseInventory()
+    {
+        inventory.SetActive(false);
+        inventoryButton.SetActive(true);
+    }
+}
