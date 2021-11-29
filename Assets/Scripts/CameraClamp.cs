7d7fb174e137ec05045f506f90c4edd7db56f280
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2021-11-29 16:47:26 +0100

    Camera follow + clamp

---
+++ Assets/Scripts/CameraClamp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClamp : MonoBehaviour
+{
+    [SerializeField] private Transform TargetToFollow;
+    [SerializeField] private float minX, maxX;
+    [SerializeField] private float minY, maxY;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(TargetToFollow.position.x, minX, maxX), 
+                                        Mathf.Clamp(TargetToFollow.position.y, minY, maxY),
+                                        transform.position.z);
+    }
+}
