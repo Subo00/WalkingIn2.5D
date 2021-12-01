22cc8ceb0928318845c2feacf15602dd864d02b5
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2021-12-01 23:28:12 +0100

    Light and camera work

--- Assets/Scripts/CameraClamp.cs
+++ Assets/Scripts/CameraClamp.cs
@@ -6,12 +6,11 @@ public class CameraClamp : MonoBehaviour
 {
     [SerializeField] private Transform TargetToFollow;
     [SerializeField] private float minX, maxX;
-    [SerializeField] private float minY, maxY;
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(Mathf.Clamp(TargetToFollow.position.x, minX, maxX), 
-                                        Mathf.Clamp(TargetToFollow.position.y, minY, maxY),
+                                        transform.position.y,
                                         transform.position.z);
     }
 }
