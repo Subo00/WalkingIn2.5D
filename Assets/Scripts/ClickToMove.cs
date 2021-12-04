206f6fd926a33a51d9a61b36806704f1a9d4313b
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2021-12-04 10:09:10 +0100

    Added textual label on hover

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -41,5 +41,6 @@ public class ClickToMove : MonoBehaviour
         animator.SetBool("isWalking",isWalking);
     }
 
-    
+   
+
 }
---
+++ Assets/Scripts/UI/MouseOverObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class MouseOverObject : MonoBehaviour
+{
+    [SerializeField] private GameObject hoverLabel;
+    [SerializeField] private string hoverMessage;
+    [SerializeField] private Dialog dialog;
+    [SerializeField] private string dialogMessage;
+    
+    public void OnMouseDown()
+    {
+        dialog.ChangeText(dialogMessage);
+    }
+    void OnMouseEnter()
+    {
+        hoverLabel.GetComponent<Text>().text = hoverMessage;
+        Cursor.visible = false;
+        hoverLabel.SetActive(true);
+    }
+
+    void OnMouseOver()
+    {
+        hoverLabel.transform.position = Input.mousePosition;
+    }
+    void OnMouseExit()
+    {
+        Cursor.visible = true;
+        hoverLabel.SetActive(false);
+    }
+
+    
+}
